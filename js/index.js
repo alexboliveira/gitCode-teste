30b796e0fa6faf7a3d051166bae2452f2ac77c2f
= <=>
2023-07-02 22:41:14 -0300

    inserindo const Pessoa

---
+++ js/index.js
@@ -0,0 +1,36 @@
+const Pessoa = function(){
+    this.pai = [
+                    {
+                        nome : null,
+                        filhos : []
+                    }
+                ]
+    
+
+
+    this.addPai = function() {
+        addPai = prompt("insira o nome do pai");
+        this.pai.push({nome : addPai, filhos : []});
+        return(
+            /*
+            console.log("iniciando add"),
+            console.log("Pai esta setado como "+ this.pai)   
+            */
+           console.log("iniciando add pai")
+        );
+    }
+
+    this.addFilho = function(){
+        qualPai = prompt("insira o nome do pai");
+        addFilho = prompt("insira o nome do filho");
+        
+        indicePai = pessoa.pai.findIndex(e => e.nome === "vilson");
+        pessoa.pai[indicePai].filhos.push(addFilho);
+
+
+    }
+    
+}
+
+const pessoa = new Pessoa();
+
